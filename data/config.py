7f8bdaeca8e9a8ef454c545a5cc849d7a4f91cbd
prsantiago <prsantiago96@gmail.com>
2021-01-27 22:42:55 -0600

    version 7

--- data/config.py
+++ data/config.py
@@ -174,10 +174,10 @@ pascal_sbd_dataset = dataset_base.copy({
 
 face_mask_dataset = dataset_base.copy({
   'name': 'Facemask',
-  'train_info': '/content/face_mask/train/coco-annotations.json',
-  'train_images': '/content/face_mask/train/images',
-  'valid_info': '/content/face_mask/val/coco-annotations.json',
-  'valid_images': '/content/face_mask/val/images',
+  'train_info': '/content/face_mask/version_7/train/coco-annotations.json',
+  'train_images': '/content/face_mask/version_7/train/images',
+  'valid_info': '/content/face_mask/version_7/val/coco-annotations.json',
+  'valid_images': '/content/face_mask/version_7/val/images',
   'has_gt': True,
   'class_names': ('con_cubrebocas', 'mal_cubrebocas', 'sin_cubrebocas', )
 })
